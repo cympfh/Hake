7b85de23e1010d78db9d7b79af730eb7e5d36eb2
cympfh <cympfh@gmail.com>
2020-08-14 23:12:15 +0900

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use std::process::Command;
+extern crate serde_json;
+use serde_json::json;
+
+extern crate chrono;
+use chrono::prelude::*;
+
+use std::io::{BufRead, BufReader, Write};
+use std::process::{Child, Command, Stdio};
 
 mod map;
 mod name;
@@ -10,13 +17,18 @@ use options::*;
 fn make(opt: &Options) -> Result<(), String> {
     let (targets, map) = opt.target_map();
     let name = opt.name();
-    eprintln!("Experiment Name: {}", &name);
+    eprintln!("\x1b[33mExperiment Name: {}\x1b[0m", &name);
 
     let mut args = vec![String::from("-f"), opt.makefile()?];
     for t in targets {
         args.push(t.clone());
     }
     args.push(format!("NAME={}", &name));
+
+    fn log_file_name(name: &String, id: usize) -> String {
+        let now = Local::now();
+        format!(".hake_log/{}_{}_{:08}", now.format("%Y%m%d"), name, id)
+    }
 
     for (id, param) in map.iter().enumerate() {
         let mut args = args.clone();
@@ -29,16 +41,50 @@ fn make(opt: &Options) -> Result<(), String> {
             };
             args.push(s);
         }
-        if opt.debug {
-            eprintln!("make id={} args={:?}", id, &args);
-        }
-        Command::new("make")
-            .args(args)
+        let log = log_file_name(&name, id);
+        eprintln!(
+            "\x1b[34mHake (NAME={}, ID={}, log=>{:?})\x1b[0m",
+            &name, id, log
+        );
+        let mut child = Command::new("make")
+            .args(&args)
+            .stdout(Stdio::piped())
             .spawn()
             .expect("Something Error to Make");
+        listen(&mut child, &name, &log, &args);
     }
 
     Ok(())
+}
+
+fn listen(child: &mut Child, name: &String, log: &String, args: &Vec<String>) {
+    use std::fs::{create_dir_all, OpenOptions};
+    create_dir_all(".hake_log").unwrap();
+    let mut log = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(log)
+        .unwrap();
+
+    let mut tee = |line: String| {
+        let now = Local::now();
+        let line = format!("[{:?}] {}\n", now, line);
+        let _ = log.write_all(line.as_bytes());
+        print!("{}", line);
+    };
+
+    tee(json!({"name": &name, "make_args": &args}).to_string());
+
+    if let Some(out) = child.stdout.as_mut() {
+        let reader = BufReader::new(out);
+        for line in reader.lines() {
+            match line {
+                Ok(line) => tee(line),
+                _ => {}
+            }
+        }
+    }
 }
 
 fn main() -> Result<(), String> {
